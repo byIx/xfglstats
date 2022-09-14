1484e861d6d3e61a54ccb0c9e9b9d7640a4d26b4
byIx <40878600+byIx@users.noreply.github.com>
2022-09-15 00:38:35 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1662919200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
+{ "version":"1.0.0", "longversion":10000, "released":1662919200, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2762-k4-xf-global-statistics\/" }
